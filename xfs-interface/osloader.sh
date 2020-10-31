2d8f58bbe23b4c772a1b134215caae56f7208a82
Ginko <ginko@pop-os.localdomain>
2020-10-31 11:13:19 +0530

    Stage 12 with some potential modifications

---
+++ xfs-interface/osloader.sh
@@ -0,0 +1,4 @@
+./xfs-interface load --os $HOME/myexpos/spl/spl_progs/os_startup.xsm
+./xfs-interface load --idle $HOME/myexpos/expl/expl_progs/idle_program.xsm
+./xfs-interface load --init $HOME/myexpos/expl/expl_progs/structprog.xsm
+./xfs-interface load --int=timer $HOME/myexpos/spl/spl_progs/sample_timer.xsm
