b35a700155f470541727018c783142c2126265f0
Ginko <ginko@pop-os.localdomain>
2020-11-02 02:41:04 +0530

    Boot module primary completed

--- xfs-interface/osloader.sh
+++ xfs-interface/osloader.sh
@@ -2,3 +2,5 @@
 ./xfs-interface load --idle $HOME/myexpos/expl/expl_progs/idle_program.xsm
 ./xfs-interface load --init $HOME/myexpos/expl/expl_progs/structprog.xsm
 ./xfs-interface load --int=timer $HOME/myexpos/spl/spl_progs/sample_timer.xsm
+./xfs-interface load --module 7 $HOME/myexpos/spl/spl_progs/module7.xsm
+
